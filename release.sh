f1dc93256bdf7e181040560213a5ac82105d1a7b
Andrew Galvez <dogisamoose.amazon@gmail.com>
2025-09-20 16:39:09 -0700

    update utils

--- release.sh
+++ release.sh
@@ -3,3 +3,8 @@ mv game build/
 zip -r crab2-linux.zip build/
 gh release new $1
 gh release upload $1 crab2-linux.zip
+
+make wasm
+mv index.* ../website-react/public/wasm/crab2
+cd ../website-react
+./update-builds.sh
